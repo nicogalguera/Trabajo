248b68ec37345976a9b5220e00755a2d8994df93
nicogalguera <UO295092@uniovi.es>
2025-11-08 22:04:23 +0100

    Single-Thread completado. Error en el cambio de color de las imagenes

--- 2acuniovi-single-thread/main.cpp
+++ 2acuniovi-single-thread/main.cpp
@@ -11,10 +11,10 @@
 using namespace cimg_library;
 
 // Data type for image components
-// FIXME: Change this type according to your group assignment
 typedef float data_t;
 
-const char* SOURCE_IMG      = "bailarina.bmp";
+const char* SOURCE1_IMG      = "hojas_2.bmp";
+const char* SOURCE2_IMG      = "background_H.bmp";
 const char* DESTINATION_IMG = "bailarina2.bmp";
 
 // Filter argument data type
@@ -22,12 +22,13 @@ typedef struct {
 	data_t *pRsrc1; // Pointers to the R, G and B components
 	data_t *pGsrc1;
 	data_t *pBsrc1;
-	data_t *pRdst;
-	data_t *pGdst;
-	data_t *pBdst;
-	data_t *pRsrc2; // Pointers to the R, G and B components
+	data_t *pRsrc2; 
 	data_t *pGsrc2;
 	data_t *pBsrc2;
+	data_t *pRdst; // Pointers to the R, G and B components
+	data_t *pGdst;
+	data_t *pBdst;
+	
 	uint pixelCount; // Size of the image in pixels
 } filter_args_t;
 
@@ -40,58 +41,67 @@ typedef struct {
  * 		
  * *********************************************/
 void filter (filter_args_t args) {
-	/************************************************
-	 * FIXME: Algorithm.
-	 * In this example, the algorithm is a components swap
-	 *
-	 * TO BE REPLACED BY YOUR ALGORITHM
-	 */
     for (uint i = 0; i < args.pixelCount; i++) {
-		*(args.pRdst + i) = *(args.pGsrc + i);  // This is equals to pRdest[i] = pGsrc[i]
-		*(args.pGdst + i) = *(args.pBsrc + i);
-		*(args.pBdst + i) = *(args.pRsrc + i);
+		*(args.pRdst + i) = 255 - (256 * (255 - *(args.pRsrc2 + i)) / (*(args.pRsrc1 + i) + 1));
+		*(args.pGdst + i) = 255 - (256 * (255 - *(args.pGsrc2 + i)) / (*(args.pGsrc1 + i) + 1));
+		*(args.pBdst + i) = 255 - (256 * (255 - *(args.pBsrc2 + i)) / (*(args.pBsrc1 + i) + 1));
 	}
 
 }
 
 int main() {
 	// Open file and object initialization
-	CImg<data_t> srcImage(SOURCE_IMG);
+	CImg<data_t> srcImage1(SOURCE1_IMG);
+	CImg<data_t> srcImage2(SOURCE2_IMG);
 
 	filter_args_t filter_args;
 	data_t *pDstImage; // Pointer to the new image pixels
 
 
 	/***************************************************
-	 * TODO: Variables initialization.
 	 *   - Prepare variables for the algorithm
 	 *   - This is not included in the benchmark time
 	 */
 
-	srcImage.display(); // Displays the source image
-	uint width = srcImage.width();// Getting information from the source image
-	uint height = srcImage.height();	
-	uint nComp = srcImage.spectrum();// source image number of components
+
+
+	srcImage1.display(); // Displays the source image1
+	uint width1 = srcImage1.width();// Getting information from the source image
+	uint height1 = srcImage1.height();	
+	uint nComp1 = srcImage1.spectrum();// source image number of components
 	         // Common values for spectrum (number of image components):
 				//  B&W images = 1
 				//	Normal color images = 3 (RGB)
 				//  Special color images = 4 (RGB and alpha/transparency channel)
+	srcImage2.display(); // Displays the source image1
+	uint width2 = srcImage2.width();// Getting information from the source image2
+	uint height2 = srcImage2.height();	
+	uint nComp2 = srcImage2.spectrum();
+
+	// Check if both images are the same size
+	if (width1 != width2 || height1 != height2){
+		perror("Images sizes aren't the same.");
+		exit(-2);
+	} 
 
 	// Calculating image size in pixels
-	filter_args.pixelCount = width * height;
+	filter_args.pixelCount = width1 * height1;
 	
 	// Allocate memory space for destination image components
-	pDstImage = (data_t *) malloc (filter_args.pixelCount * nComp * sizeof(data_t));
+	pDstImage = (data_t *) malloc (filter_args.pixelCount * nComp1 * sizeof(data_t));
 	if (pDstImage == NULL) {
 		perror("Allocating destination image");
 		exit(-2);
 	}
 
-	// Pointers to the componet arrays of the source image
-	filter_args.pRsrc = srcImage.data(); // pRcomp points to the R component array
-	filter_args.pGsrc = filter_args.pRsrc + filter_args.pixelCount; // pGcomp points to the G component array
-	filter_args.pBsrc = filter_args.pGsrc + filter_args.pixelCount; // pBcomp points to B component array
-	
+	// Pointers to the componet arrays of the source images
+	filter_args.pRsrc1 = srcImage1.data(); // pRcomp points to the R component array
+	filter_args.pGsrc1 = filter_args.pRsrc1 + filter_args.pixelCount; // pGcomp points to the G component array
+	filter_args.pBsrc1 = filter_args.pGsrc1 + filter_args.pixelCount; // pBcomp points to B component array
+	filter_args.pRsrc2 = srcImage2.data(); 
+	filter_args.pGsrc2 = filter_args.pRsrc2 + filter_args.pixelCount; 
+	filter_args.pBsrc2 = filter_args.pGsrc2 + filter_args.pixelCount; 
+
 	// Pointers to the RGB arrays of the destination image
 	filter_args.pRdst = pDstImage;
 	filter_args.pGdst = filter_args.pRdst + filter_args.pixelCount;
@@ -99,10 +109,15 @@ int main() {
 
 
 	/***********************************************
-	 * TODO: Algorithm start.
 	 *   - Measure initial time
 	 */
+	struct timespec tStart, tEnd;
+	double dElapsedTimeS;
 
+	if (clock_gettime(CLOCK_REALTIME, &tStart) == -1){
+		perror("clock_gettime");
+		exit(EXIT_FAILURE);
+	}
 
 	/************************************************
 	 * Algorithm.
@@ -110,17 +125,29 @@ int main() {
 	filter(filter_args);
 
 
-	/***********************************************
-	 * TODO: End of the algorithm.
+
+
+	/************************************************
 	 *   - Measure the end time
 	 *   - Calculate the elapsed time
 	 */
+	if (clock_gettime(CLOCK_REALTIME, &tEnd) == -1)
+	{
+		perror("clock_gettime");
+		exit(EXIT_FAILURE);
+	}
 
-		
+
+	// Show the elapsed time
+	dElapsedTimeS = (tEnd.tv_sec - tStart.tv_sec);
+        dElapsedTimeS += (tEnd.tv_nsec - tStart.tv_nsec) / 1e+9;
+	printf("Elapsed time    : %f s.\n", dElapsedTimeS);
+
+
 	// Create a new image object with the calculated pixels
 	// In case of normal color images use nComp=3,
 	// In case of B/W images use nComp=1.
-	CImg<data_t> dstImage(pDstImage, width, height, 1, nComp);
+	CImg<data_t> dstImage(pDstImage, width1, height1, 1, nComp1);
 
 	// Store destination image in disk
 	dstImage.save(DESTINATION_IMG); 
